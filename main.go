c7fb2db427a8cb46ba422dbe7e2b879324ee3d43
Anastasios Gogos <angogos@iit.demokritos.gr>
2016-12-19 14:40:34 +0200

    works for cpu usage

--- main.go
+++ main.go
@@ -1,30 +1,12 @@
 package main
 
 import (
-    "log"
-    "fmt"
-    "time"
-    linuxproc "github.com/c9s/goprocinfo/linux"
+	"fmt"
+	"log"
+	"time"
+
+	linuxproc "github.com/c9s/goprocinfo/linux"
 )
-
-
-// source: http://stackoverflow.com/questions/23367857/accurate-calculation-of-cpu-usage-given-in-percentage-in-linux
-
-// PrevIdle = previdle + previowait
-// Idle = idle + iowait
-//
-// PrevNonIdle = prevuser + prevnice + prevsystem + previrq + prevsoftirq + prevsteal
-// NonIdle = user + nice + system + irq + softirq + steal
-//
-// PrevTotal = PrevIdle + PrevNonIdle
-// Total = Idle + NonIdle
-//
-// # differentiate: actual value minus the previous one
-// totald = Total - PrevTotal
-// idled = Idle - PrevIdle
-//
-// CPU_Percentage = (totald - idled)/totald
-
 
 // type CPUStat struct {
 //     Id        string `json:"id"`
@@ -41,52 +23,107 @@ import (
 // }
 
 type MyCPUStats struct {
-  Idle      int
-  NonIdle   int
-  Total     int
+	Cpu0 float32
+	Cpu1 float32
+	Cpu2 float32
+	Cpu3 float32
 }
-
 
 func main() {
 
-  var prevCPUStats *linuxproc.CPUStat
-  var currCPUStats *linuxproc.CPUStat
+	//var prevCPUStats linuxproc.CPUStat
+	//var currCPUStats linuxproc.CPUStat
 
-  currCPUStats := ReadCPUStats()
-  prevCPUStats := ReadCPUStats()
+	currCPUStats := ReadCPUStats()
+	prevCPUStats := ReadCPUStats()
 
-  for {
-    time.Sleep(time.Second * 10)
+	for {
+		time.Sleep(time.Second * 4)
 
-    currCPUStats := ReadCPUStats()
+		currCPUStats = ReadCPUStats()
+		calcMyCPUStats(currCPUStats, prevCPUStats)
+		prevCPUStats = currCPUStats
 
-
-    time.Sleep(time.Second * 10)
-  }
+	}
 
 }
-
 
 func ReadCPUStats() *linuxproc.Stat {
-  stat, err := linuxproc.ReadStat("/proc/stat")
-  if err != nil {
-      log.Fatal("stat read fail")
-  }
-  //fmt.Println(stat)
-  return stat
+	stat, err := linuxproc.ReadStat("/proc/stat")
+	if err != nil {
+		log.Fatal("stat read fail")
+	}
+	// fmt.Println(stat)
+	return stat
 
-  // for _, s := range stat.CPUStats {
-  //     // s.User
-  //     // s.Nice
-  //     // s.System
-  //     // s.Idle
-  //     // s.IOWait
-  //     fmt.Println(s)
-  // }
+	// for _, s := range stat.CPUStats {
+	//     // s.User
+	//     // s.Nice
+	//     // s.System
+	//     // s.Idle
+	//     // s.IOWait
+	//     fmt.Println(s)
+	// }
 }
 
+func calcMyCPUStats(curr, prev *linuxproc.Stat) *MyCPUStats {
 
-func calcMyCPUStats() *MyCPUStats {
-  stats := ReadCPUStats()
-  
+	var stats MyCPUStats
+
+	// fmt.Println("cpu0: ", calcSingleCoreUsage(curr.CPUStats[0], prev.CPUStats[0]))
+	// fmt.Println("cpu1: ", calcSingleCoreUsage(curr.CPUStats[1], prev.CPUStats[1]))
+	// fmt.Println("cpu2: ", calcSingleCoreUsage(curr.CPUStats[2], prev.CPUStats[2]))
+	// fmt.Println("cpu3: ", calcSingleCoreUsage(curr.CPUStats[3], prev.CPUStats[3]))
+
+	stats.Cpu0 = calcSingleCoreUsage(curr.CPUStats[0], prev.CPUStats[0])
+	stats.Cpu1 = calcSingleCoreUsage(curr.CPUStats[1], prev.CPUStats[1])
+	stats.Cpu2 = calcSingleCoreUsage(curr.CPUStats[2], prev.CPUStats[2])
+	stats.Cpu3 = calcSingleCoreUsage(curr.CPUStats[3], prev.CPUStats[3])
+
+	fmt.Printf("Stats:\n%+v", stats)
+
+	return &stats
+}
+
+func calcSingleCoreUsage(curr, prev linuxproc.CPUStat) float32 {
+
+	/*
+	 *    source: http://stackoverflow.com/questions/23367857/accurate-calculation-of-cpu-usage-given-in-percentage-in-linux
+	 *
+	 *    PrevIdle = previdle + previowait
+	 *    Idle = idle + iowait
+	 *
+	 *    PrevNonIdle = prevuser + prevnice + prevsystem + previrq + prevsoftirq + prevsteal
+	 *    NonIdle = user + nice + system + irq + softirq + steal
+	 *
+	 *    PrevTotal = PrevIdle + PrevNonIdle
+	 *    Total = Idle + NonIdle
+	 *
+	 *    # differentiate: actual value minus the previous one
+	 *    totald = Total - PrevTotal
+	 *    idled = Idle - PrevIdle
+	 *
+	 *    CPU_Percentage = (totald - idled)/totald
+	 */
+
+	// fmt.Printf("curr:\n %+v\n", curr)
+	// fmt.Printf("prev:\n %+v\n", prev)
+
+	PrevIdle := prev.Idle + prev.IOWait
+	Idle := curr.Idle + curr.IOWait
+
+	PrevNonIdle := prev.User + prev.Nice + prev.System + prev.IRQ + prev.SoftIRQ + prev.Steal
+	NonIdle := curr.User + curr.Nice + curr.System + curr.IRQ + curr.SoftIRQ + curr.Steal
+
+	PrevTotal := PrevIdle + PrevNonIdle
+	Total := Idle + NonIdle
+	// fmt.Println(PrevIdle, Idle, PrevNonIdle, NonIdle, PrevTotal, Total)
+
+	//  differentiate: actual value minus the previous one
+	totald := Total - PrevTotal
+	idled := Idle - PrevIdle
+
+	CPU_Percentage := (float32(totald) - float32(idled)) / float32(totald)
+
+	return CPU_Percentage
 }
